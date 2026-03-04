c59036bdeab521572e9170f8700ef9526fcc1693
indresh756606 <113124112+indresh756606@users.noreply.github.com>
2026-03-05 01:13:53 +0530

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://YOUR_PROJECT_ID.supabase.co"
-const supabaseKey = "YOUR_PUBLIC_ANON_KEY"
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
